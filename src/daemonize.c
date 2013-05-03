61316adc765f035915e81aab6900da57c2fd3352
stnuessl <bockler@gmx.net>
2013-05-03 18:26:03 +0200

    initalizing struct sigaction now (surpresses valgrind warnings)

--- src/daemonize.c
+++ src/daemonize.c
@@ -46,6 +46,8 @@ static int _ignore_signals(void)
     int num_signals;
     struct sigaction sa;
     
+    memset(&sa, 0, sizeof(sa));
+    
     sa.sa_flags   = SA_SIGINFO;
     sa.sa_handler = SIG_IGN;
     
